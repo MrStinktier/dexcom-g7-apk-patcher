199a94b8b58e9ba0df72d5c864aebd23bf096398
MrStinktier <144330316+MrStinktier@users.noreply.github.com>
2026-06-02 22:46:39 +0200

    Update download link for dexcom stock XAPK

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -8,7 +8,7 @@ cd ..
 
 wget  --user-agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 Chrome/137.0.0.0 Safari/537.36' \
       -O dexcom.stock.xapk \
-      https://d.apkpure.com/b/APK/com.dexcom.g7?version=1.6.1.4537
+      https://d.apkpure.com/b/XAPK/com.dexcom.g7?version=latest
 
 python ./xapk-to-apk/xapktoapk.py dexcom.stock.xapk
 
