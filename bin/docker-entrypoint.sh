8b772d8e5741e49f20fbd372462e54d0fcbfa5e4
MrStinktier <144330316+MrStinktier@users.noreply.github.com>
2026-06-02 22:37:32 +0200

    Update download URL for dexcom stock APK

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -2,6 +2,6 @@
 
 wget  --user-agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 Chrome/137.0.0.0 Safari/537.36' \
       -O dexcom.stock.apk \
-      https://d.apkpure.com/b/APK/com.dexcom.g7?versionCode=4537
+      https://d.apkpure.com/b/XAPK/com.dexcom.g7?version=latest
 bin/build.sh ./dexcom.stock.apk
 mv dexcom.patched.apk /output
