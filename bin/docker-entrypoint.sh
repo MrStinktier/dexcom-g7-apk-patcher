e2ae9010a69eea3565469815d340b7a8cf0c9a85
MrStinktier <144330316+MrStinktier@users.noreply.github.com>
2026-06-02 22:15:11 +0200

    Update user-agent string in docker-entrypoint.sh

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget  --user-agent='Mozilla/5.0' \
+wget  --user-agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 Chrome/137.0.0.0 Safari/537.36' \
       -O dexcom.stock.apk \
       https://d.apkpure.com/b/APK/com.dexcom.g7?versionCode=4537
 bin/build.sh ./dexcom.stock.apk
