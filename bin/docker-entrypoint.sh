b689c05a07e85eff106c9319f7eecaeba6938610
MrStinktier <144330316+MrStinktier@users.noreply.github.com>
2026-06-02 22:42:12 +0200

    Update docker-entrypoint to handle XAPK files
    Replaced direct APK download with XAPK processing using a script.

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-wget  --user-agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 Chrome/137.0.0.0 Safari/537.36' \
-      -O dexcom.stock.apk \
-      https://d.apkpure.com/b/XAPK/com.dexcom.g7?version=latest
+git clone https://github.com/LuigiVampa92/xapk-to-apk
+cd xapk-to-apk
+chmod +x xapktoapk.py
+
+cd ..
+
+wget  --user-agent='Mozilla/5.0' \
+      -O dexcom.stock.xapk \
+      https://d.apkpure.com/b/APK/com.dexcom.g7?version=1.6.1.4537
+
+python ./xapk-to-apk/xapktoapk.py dexcom.stock.xapk
+
 bin/build.sh ./dexcom.stock.apk
 mv dexcom.patched.apk /output
